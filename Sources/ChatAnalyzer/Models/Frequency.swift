079546830b9163dea05b0870f2980bdd078478f4
Junpeng Chen <44418854+ChanChunPang@users.noreply.github.com>
2023-09-22 23:31:50 +0200

    Make Frequency enum conform to CaseIterable and CustomStringConvertible

--- Sources/ChatAnalyzer/Models/Frequency.swift
+++ Sources/ChatAnalyzer/Models/Frequency.swift
@@ -5,11 +5,22 @@
 //  Created by Junpeng Chen on 07.09.23.
 //
 
-public enum Frequency: Codable {
+public enum Frequency: CaseIterable, Codable, CustomStringConvertible {
     case year
     case month
     case week
     case day
     case weekday
     case hour
+    
+    public var description : String {
+      switch self {
+      case .year: return "Year"
+      case .month: return "Month"
+      case .week: return "Week"
+      case .day: return "Day"
+      case .weekday: return "Weekday"
+      case .hour: return "Hour"
+      }
+    }
 }
